4c049f96b26c46db7b33f371992ea5f6a8028c81
elmurodWeb <elmurodorinboyev@gmail.com>
2023-09-07 14:32:45 +0500

    Add resources, button, Wrapper, and UI components

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <the-header title="Remember Me"></the-header>
+  <StoredResource :resources="storedResources"></StoredResource>
+</template>
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+import StoredResource from './components/learning-resources/StoredResource.vue';
+export default {
+  components: {
+    StoredResource,
+    TheHeader,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'The official guide',
+          description: 'More information in the website.',
+          link: 'https//:vue.org',
+        },
+        {
+          id: 'gooogle',
+          title: 'The Google',
+          description: 'To know more information',
+          link: 'https//:google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
